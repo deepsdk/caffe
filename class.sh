0e58f8800a8eb302f6a0e2db79335f2575353a05
deepsdk <deepsdk.io@gmail.com>
2016-10-24 16:54:29 +0900

    test build-in imagenet models

---
+++ class.sh
@@ -0,0 +1,25 @@
+set -e
+
+#./build/example/caffe test -iterations 1 -model "../model/lenet_id_data_layer.prototxt" -weights "../model/lenet_iter_10000.caffemodel" $@  #--minloglevel=1
+
+MEAN="./data/ilsvrc12/imagenet_mean.binaryproto"
+LABELS="./data/ilsvrc12/synset_words.txt"
+
+DEPLOY="./models/bvlc_reference_caffenet/deploy.prototxt"
+MODEL="./models/bvlc_reference_caffenet/bvlc_reference_caffenet.caffemodel"
+echo $MODEL
+.build_release/examples/cpp_classification/classification.bin $DEPLOY $MODEL $MEAN $LABELS $@
+
+echo ""
+
+DEPLOY="./models/bvlc_googlenet/deploy.prototxt"
+MODEL="./models/bvlc_googlenet/bvlc_googlenet.caffemodel"
+echo $MODEL
+.build_release/examples/cpp_classification/classification.bin $DEPLOY $MODEL $MEAN $LABELS $@
+
+echo ""
+
+DEPLOY="./models/bvlc_alexnet/deploy.prototxt"
+MODEL="./models/bvlc_alexnet/bvlc_alexnet.caffemodel"
+echo $MODEL
+.build_release/examples/cpp_classification/classification.bin $DEPLOY $MODEL $MEAN $LABELS $@
