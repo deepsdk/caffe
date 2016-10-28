f7c8ca22f48afd2618ef7d44d629b8d44c23b677
deepsdk <deepsdk.io@gmail.com>
2016-10-28 16:25:01 +0900

    add vgg and nin model

--- app_update.sh
+++ app_update.sh
@@ -1,4 +1,3 @@
 scp build/tools/caffe.bin lambda:~/package/bin
-scp build/tools/lambda.bin lambda:~/package/bin
 scp build/lib/libcaffe.so.1.0.0-rc3 lambda:~/package/lib
 scp build/examples/cpp_classification/classification.bin lambda:~/package/bin
--- class.sh
+++ class.sh
@@ -1,32 +1,48 @@
 set -e
 
+make all -j4
+
 #./build/example/caffe test -iterations 1 -model "../model/lenet_id_data_layer.prototxt" -weights "../model/lenet_iter_10000.caffemodel" $@  #--minloglevel=1
 
 MEAN="./data/ilsvrc12/imagenet_mean.binaryproto"
 LABELS="./data/ilsvrc12/synset_words.txt"
 
-#DEPLOY="./models/bvlc_reference_caffenet/deploy.prototxt"
-#MODEL="./models/bvlc_reference_caffenet/bvlc_reference_caffenet.caffemodel"
-#echo $MODEL
-#build/examples/cpp_classification/classification.bin $DEPLOY $MODEL $MEAN $LABELS $@
-#
-#echo ""
-#
-#DEPLOY="./models/bvlc_googlenet/deploy.prototxt"
-#MODEL="./models/bvlc_googlenet/bvlc_googlenet.caffemodel"
-#echo $MODEL
-#build/examples/cpp_classification/classification.bin $DEPLOY $MODEL $MEAN $LABELS $@
-
-#echo ""
-#
-#DEPLOY="./models/bvlc_alexnet/deploy.prototxt"
-#MODEL="./models/bvlc_alexnet/bvlc_alexnet.caffemodel"
-#echo $MODEL
-#build/examples/cpp_classification/classification.bin $DEPLOY $MODEL $MEAN $LABELS $@
+DEPLOY="./models/bvlc_reference_caffenet/deploy.prototxt"
+MODEL="./models/bvlc_reference_caffenet/bvlc_reference_caffenet.caffemodel"
+echo $MODEL
+build/examples/cpp_classification/classification.bin $DEPLOY $MODEL $MEAN $LABELS $@
 
 echo ""
 
-DEPLOY="./models/zoo_nin_imagenet/train_val.prototxt"
+DEPLOY="./models/bvlc_googlenet/deploy.prototxt"
+MODEL="./models/bvlc_googlenet/bvlc_googlenet.caffemodel"
+echo $MODEL
+build/examples/cpp_classification/classification.bin $DEPLOY $MODEL $MEAN $LABELS $@
+
+echo ""
+
+DEPLOY="./models/bvlc_alexnet/deploy.prototxt"
+MODEL="./models/bvlc_alexnet/bvlc_alexnet.caffemodel"
+echo $MODEL
+build/examples/cpp_classification/classification.bin $DEPLOY $MODEL $MEAN $LABELS $@
+
+echo ""
+
+DEPLOY="./models/zoo_nin_imagenet/deploy.prototxt"
 MODEL="./models/zoo_nin_imagenet/nin_imagenet.caffemodel"
+echo $MODEL
+build/examples/cpp_classification/classification.bin $DEPLOY $MODEL $MEAN $LABELS $@
+
+echo ""
+
+DEPLOY="./models/zoo_vgg_ilsvrc_16_layers/VGG_ILSVRC_16_layers_deploy.prototxt"
+MODEL="./models/zoo_vgg_ilsvrc_16_layers/VGG_ILSVRC_16_layers.caffemodel"
+echo $MODEL
+build/examples/cpp_classification/classification.bin $DEPLOY $MODEL $MEAN $LABELS $@
+
+echo ""
+
+DEPLOY="./models/zoo_vgg_ilsvrc_19_layers/VGG_ILSVRC_19_layers_deploy.prototxt"
+MODEL="./models/zoo_vgg_ilsvrc_19_layers/VGG_ILSVRC_19_layers.caffemodel"
 echo $MODEL
 build/examples/cpp_classification/classification.bin $DEPLOY $MODEL $MEAN $LABELS $@
